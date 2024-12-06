30b390f4f21a8d442a11b158ce1527b829cc1b5e
Viet281101 <nva281101@gmail.com>
2024-12-06 14:07:24 +0700

    start header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "../styles/components/header.scss";
+import logo from "../assets/images/logo.png";
 
 function Header() {
 	return (
 		<header className="header">
-			<h1>Welcome to Beauty Clinic</h1>
+		<a href="/" className="logo">
+			<img src={logo} alt="Beautice Icon" className="logo-icon" />
+			<span className="logo-text">Beautice</span>
+		</a>
+		<nav className="main-menu">
+			<ul>
+			<li><a href="/" className="home active">Home</a></li>
+			<li><a href="/" className="about">About</a></li>
+			<li><a href="/" className="service">Service</a></li>
+			<li><a href="/" className="gallery">Gallery</a></li>
+			<li><a href="/" className="blog">Blog</a></li>
+			</ul>
+		</nav>
+		<button className="contact-button">Contact</button>
 		</header>
 	);
 }
