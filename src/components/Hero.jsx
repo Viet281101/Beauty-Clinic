456b98e37c234b7d16a4d83886f53c6ecd6bdb07
Viet281101 <nva281101@gmail.com>
2024-12-06 16:57:41 +0700

    start hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import "../styles/components/hero.scss";
+import heroImage from "../assets/bg_frame_1.svg";
 
 function Hero() {
 	return (
-		<section>
-		<h2>Hero Section</h2>
+		<section className="hero" id="hero">
+		<div className="hero-content">
+			<h1>Clinic & beauty<br/>consultant</h1>
+			<p>It is a long established fact that a reader will be<br/>by the readable content of a page.</p>
+			<button>More Details</button>
+		</div>
+		<div className="hero-image">
+			<img src={heroImage} alt="Background Frame" />
+		</div>
 		</section>
 	);
 }
