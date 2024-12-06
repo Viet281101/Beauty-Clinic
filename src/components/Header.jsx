e7670e245377a1f3e5ccf0f9e0ddd2e18307942b
Viet281101 <nva281101@gmail.com>
2024-12-06 19:34:28 +0700

    header style to tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,21 @@ import logo from "../assets/images/main_logo.png";
 
 function Header() {
 	return (
-		<header className="header">
-		<a href="/" className="logo">
+		<header className="header w-4/5 flex p-0 justify-between items-center">
+		<a href="/" className="logo relative">
 			<img src={logo} alt="Beautice Icon" className="logo-icon" />
-			<span className="logo-text">Beautice</span>
+			<span className="logo-text font-bold">Beautice</span>
 		</a>
-		<nav className="main-menu">
-			<ul>
-			<li><a href="/" className="home active">Home</a> +</li>
-			<li><a href="/" className="about">About</a></li>
-			<li><a href="/" className="service">Service</a></li>
-			<li><a href="/" className="gallery">Gallery</a></li>
-			<li><a href="/" className="blog">Blog</a></li>
+		<nav className="main-menu relative">
+			<ul className="flex w-full justify-around font-semibold">
+			<li><a href="/" className="home active relative">Home</a> +</li>
+			<li><a href="/" className="about relative left-0">About</a></li>
+			<li><a href="/" className="service relative top-0">Service</a></li>
+			<li><a href="/" className="gallery relative left-0 top-0">Gallery</a></li>
+			<li><a href="/" className="blog relative">Blog</a></li>
 			</ul>
 		</nav>
-		<button className="contact-button">Contact</button>
+		<button className="contact-button relative font-semibold">Contact</button>
 		</header>
 	);
 }
