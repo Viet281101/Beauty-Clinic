d683087b1ce057915b8c62c76c5efdef3b9b69ca
Viet281101 <nva281101@gmail.com>
2024-12-06 10:38:41 +0700

    fix setup files

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-	return (
-		<div>
-			<h1>Welcome to Beauty Clinic</h1>
-		</div>
-	);
-}
-
-export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/components/header.scss";
+
+function Header() {
+	return (
+		<header className="header">
+			<h1>Welcome to Beauty Clinic</h1>
+		</header>
+	);
+}
+
+export default Header;
